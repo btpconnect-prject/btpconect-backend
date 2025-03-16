3cfc38192d71d237927159ddf19ab7ea143e8c51
joel polla <pollajoel2017@gmail.com>
2025-03-17 00:12:04 +0100

    fixtures for category load

--- config/bundles.php
+++ config/bundles.php
@@ -15,6 +15,6 @@ return [
     HWI\Bundle\OAuthBundle\HWIOAuthBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     FOS\ElasticaBundle\FOSElasticaBundle::class => ['all' => true],
-    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ["all" => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
 ];
