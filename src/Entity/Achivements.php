2f6954bf8765cb30014d967baabe6c51d0895b3f
joel polla <pollajoel2017@gmail.com>
2025-04-04 09:30:45 +0200

    remove cors

---
+++ src/Entity/Achivements.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\Delete;
+use ApiPlatform\Metadata\Put;
+use ApiPlatform\Metadata\Post;
+use App\Repository\AchivementsRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ApiResource(
+    normalizationContext: ['groups' => ['achievement::read']],
+    paginationItemsPerPage: 20, // Nombre d'éléments par page
+    paginationMaximumItemsPerPage: 100, // Nombre maximum d'éléments par page 
+    paginationEnabled: true, // Activer la pagination
+    operations: [
+        new GetCollection(uriTemplate: "/achievements", forceEager: false),
+        new Get(uriTemplate: "/achievement/{id}", forceEager: false),
+        new Post(uriTemplate: "/achievement"),
+        new Put(
+            uriTemplate: "/achievement/{id}",
+            forceEager: false
+        ),
+        new Delete(
+            uriTemplate: "/achievement/{id}",
+            forceEager: false,
+        )
+    ]
+)]
+#[ORM\Entity(repositoryClass: AchivementsRepository::class)]
+class Achivements
+{
+
+    use UuidTrait;
+
+    #[ORM\Column(length: 255)]
+    private ?string $videosUrl = null;
+
+
+    public function getVideosUrl(): ?string
+    {
+        return $this->videosUrl;
+    }
+
+    public function setVideosUrl(string $videosUrl): static
+    {
+        $this->videosUrl = $videosUrl;
+
+        return $this;
+    }
+}
