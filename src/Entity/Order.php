7a58a48b0b89ad210c1d054fbb388039a88cdeed
joel polla <pollajoel2017@gmail.com>
2025-04-25 14:46:47 +0200

    add order entity

--- src/Entity/Order.php
+++ src/Entity/Order.php
@@ -106,6 +106,31 @@ class Order
 
 
 
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $createdAt): static
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeImmutable
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(\DateTimeImmutable $updatedAt): static
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+
     /**
      * @see UserInterface
      */
