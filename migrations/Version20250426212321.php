bdd8405ea0a5224f5f23ddd02d3b499a877281e1
joel polla <pollajoel2017@gmail.com>
2025-04-28 08:53:15 +0200

    update nexjsconfig

--- migrations/Version20250426212321.php
+++ migrations/Version20250426212321.php
@@ -21,7 +21,7 @@ final class Version20250426212321 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE "order" ADD statstatus VARCHAR(255) DEFAULT NULL');
-        $this->addSql('ALTER TABLE "order" ALTER cart SET NOT NULL');
+        $this->addSql('ALTER TABLE "order" ALTER cart SET DEFAULT NULL');
         $this->addSql('ALTER TABLE user_entity DROP statstatus');
     }
 
