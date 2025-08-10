b162f5ee61ac522879ec859560bb8d85a0c4a8e5
joel polla <pollajoel2017@gmail.com>
2025-08-10 11:05:02 +0200

    optimize dockerfile with docker-slim

--- src/State/MediaProcessor.php
+++ src/State/MediaProcessor.php
@@ -26,41 +26,14 @@ class MediaProcessor implements ProcessorInterface
     {
         // Vérifier si nous traitons une suppression (DELETE)
         if ($operation instanceof DeleteOperationInterface && $data instanceof MediaObject) {
-
-            // 1. On cherche tous les produits qui utilisent ce MediaObject
-            /** @var ProductEntity[] */
-            $products = $this->entityManager->getRepository(ProductEntity::class)->findAll();
-
-            foreach ($products as $product) {
-                $modified = false;
-
-                // Cas 1 : image principale
-                if ($product->getImage()?->getId() === $data->getId()) {
-                    $product->setImage(null);
-                    $modified = true;
-                }
-
-                // Cas 2 : shots (galerie)
-                if ($product->getShots() && $product->getShots()->contains($data)) {
-                    $product->removeShot($data);
-                    $modified = true;
-                }
-
-                if ($modified) {
-                    $this->entityManager->persist($product);
-                }
-            }
-
-            // 2. On flush les changements
+            $id = $uriVariables['id'] ?? null;
+            /** @var MediaObject */
+            $media = $this->entityManager->getRepository(MediaObject::class)
+                ->find($id);  // Trouver le produit par son ID
+            $media->setImageNotExist(true);
             $this->entityManager->flush();
-
-            // 3. On peut maintenant supprimer le MediaObject
-            $this->entityManager->remove($data);
-            $this->entityManager->flush();
-
-            return null;
+            return  $media;
         }
-
         // Appeler le processeur par défaut (continue la suppression du produit)
         return $this->processor->process($data, $operation, $uriVariables, $context);
     }
